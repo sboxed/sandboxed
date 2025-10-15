859a16b3bf4cd0ee56a2d3b3d4ec672bfcf9d966
Vadim Melnikov <amelvspace@gmail.com>
2025-10-15 16:11:29 +0300

    test: fix failing tests

--- packages/sandboxed_core/test/multi_params_test.dart
+++ packages/sandboxed_core/test/multi_params_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandboxed_core/sandboxed_core.dart';
 
@@ -7,20 +8,70 @@ void main() {
   late ParamStorage params;
 
   setUp(() {
-    params = ParamStorage();
+    params = ParamStorage(
+      editors3: [
+        _ParamEditorFactory(),
+      ],
+    );
   });
 
-  test("params.multi() works", () {
+  test("params.multi() sets default chips editor", () {
     params.multi("id", [1, 2, 3]).required([1]);
 
     expect(params.get("id")?.value, [1]);
-    expect(params.get("id")?.editor, isA<MultiChipsEditor>());
+    expect(params.get("id")?.editor, isA<_MultiChipsEditorPresenter>());
   });
 
   test("segmented changes editor", () {
     params.multi("id", [1, 2, 3]).segmented().required([1]);
 
     expect(params.get("id")?.value, [1]);
-    expect(params.get("id")?.editor, isA<MultiSegmentedEditor>());
+    expect(params.get("id")?.editor, isA<_MultiSegmentedEditorPresenter>());
   });
+}
+
+class _ParamEditorFactory
+    extends ParamEditorFactory<ValueEditorPresenter<dynamic>?> {
+  const _ParamEditorFactory();
+
+  @override
+  ValueEditorPresenter<dynamic>? createListEditor<TItem>(
+    ListParamEditor<TItem> e,
+  ) {
+    switch (e) {
+      case MultiChipsEditor<TItem>():
+        return _MultiChipsEditorPresenter<TItem>();
+
+      case MultiSegmentedEditor<TItem>():
+        return _MultiSegmentedEditorPresenter<TItem>();
+
+      default:
+        return null;
+    }
+  }
+
+  @override
+  ValueEditorPresenter? create<T>(ParamEditor e) {
+    return null;
+  }
+}
+
+class _MultiChipsEditorPresenter<TItem>
+    extends ValueEditorPresenter<List<TItem>> {
+  const _MultiChipsEditorPresenter();
+
+  @override
+  Widget build(BuildContext context, ParamValue<List<TItem>> value) {
+    throw UnimplementedError();
+  }
+}
+
+class _MultiSegmentedEditorPresenter<TItem>
+    extends ValueEditorPresenter<List<TItem>> {
+  const _MultiSegmentedEditorPresenter();
+
+  @override
+  Widget build(BuildContext context, ParamValue<List<TItem>> value) {
+    throw UnimplementedError();
+  }
 }
--- packages/sandboxed_core/test/single_params_test.dart
+++ packages/sandboxed_core/test/single_params_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sandboxed_core/sandboxed_core.dart';
 
@@ -7,19 +8,68 @@ void main() {
   late ParamStorage params;
 
   setUp(() {
-    params = ParamStorage();
+    params = ParamStorage(
+      editors3: [
+        _ParamEditorFactory(),
+      ],
+    );
   });
 
   test("params.multi() works", () {
     params.single("id", [1, 2, 3]).required(1);
     expect(params.get("id")?.value, 1);
-    expect(params.get("id")?.editor, isA<SingleChipsEditor>());
+    expect(params.get("id")?.editor, isA<_SingleChipsEditorPresenter>());
   });
 
   test("segmented changes editor", () {
     final value = params.single("id", [1, 2, 3]).segmented().required(1);
 
     expect(value, 1);
-    expect(params.get("id")?.editor, isA<SingleSegmentedEditor>());
+    expect(params.get("id")?.editor, isA<_SingleSegmentedEditorPresenter>());
   });
+}
+
+class _ParamEditorFactory
+    extends ParamEditorFactory<ValueEditorPresenter<dynamic>?> {
+  const _ParamEditorFactory();
+
+  @override
+  ValueEditorPresenter<dynamic>? createListEditor<TItem>(
+    ListParamEditor<TItem> e,
+  ) {
+    return null;
+  }
+
+  @override
+  ValueEditorPresenter? create<T>(ParamEditor e) {
+    switch (e) {
+      case SingleChipsEditor<T>():
+        return _SingleChipsEditorPresenter<T>();
+
+      case SingleSegmentedEditor<T>():
+        return _SingleSegmentedEditorPresenter<T>();
+
+      default:
+        return null;
+    }
+  }
+}
+
+class _SingleChipsEditorPresenter<TItem> extends ValueEditorPresenter<TItem> {
+  const _SingleChipsEditorPresenter();
+
+  @override
+  Widget build(BuildContext context, ParamValue<TItem> value) {
+    throw UnimplementedError();
+  }
+}
+
+class _SingleSegmentedEditorPresenter<TItem>
+    extends ValueEditorPresenter<TItem> {
+  const _SingleSegmentedEditorPresenter();
+
+  @override
+  Widget build(BuildContext context, ParamValue<TItem> value) {
+    throw UnimplementedError();
+  }
 }
